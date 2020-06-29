45710ee5c4c2cfef9e3780e906ec3bf3bdcc8fa6
ohansyah <ohansyah@weekendinc.com>
2020-06-29 23:32:14 +0700

    [MODIFY] respond + change code to int + ResSuccess + ResError

--- dtos/response.go
+++ dtos/response.go
@@ -3,7 +3,7 @@ package dtos
 // Response as stuct
 type Response struct {
 	APIVersion string      `json:"api_version"`
-	Code       string      `json:"code"`
+	Code       int         `json:"code"`
 	Success    bool        `json:"success"`
 	Message    string      `json:"message"`
 	Data       interface{} `json:"data"`
---
+++ responds/responds.go
@@ -0,0 +1,27 @@
+package responds
+
+import (
+	"api_olshop/dtos"
+	"encoding/json"
+	"net/http"
+
+	"github.com/spf13/viper"
+)
+
+// ResSuccess return data
+func ResSuccess(w http.ResponseWriter, data dtos.Response) {
+	data.APIVersion = viper.Get("api_version").(string)
+	data.Code = 200
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}
+
+// ResErr return data
+func ResErr(w http.ResponseWriter, data dtos.Response, code int) {
+	data.APIVersion = viper.Get("api_version").(string)
+	data.Code = code
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(data)
+}
